de6d1ff150b13a2e434622ef62b92df5eb876f6b
ktakamat <kkotatakamatsu0126@gmail.com>
2024-07-14 19:58:12 +0900

    134

--- minishell/srcs/pipe_redirect/redirect.c
+++ minishell/srcs/pipe_redirect/redirect.c
@@ -6,7 +6,7 @@
 /*   By: ktakamat <ktakamat@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 22:51:54 by ktakamat          #+#    #+#             */
-/*   Updated: 2024/07/14 16:21:18 by ktakamat         ###   ########.fr       */
+/*   Updated: 2024/07/14 19:54:51 by ktakamat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,30 +50,50 @@ static void	here_doc(t_redirect *redi, int pipefd[2])
 	ft_free(res);
 }
 
+// void	redirect(t_redirect *redi)
+// {
+// 	int	pipefd[2];
+// 	int	backup;
+
+// 	if (redi->type == INPUT_REDI || redi->type == OUTPUT_REDI
+// 		|| redi->type == APPEND_OUTPUT_REDI)
+// 	{
+// 		redi->fd_backup = ft_dup(redi->fd);
+// 		ft_dup2 (redi->fd_file, redi->fd);
+// 		return ;
+// 	}
+// 	else
+// 	{
+// 		backup = ft_dup(STDIN_FILENO);
+// 		ft_pipe(pipefd);
+// 		here_doc(redi, pipefd);
+// 		ft_dup2(pipefd[0], STDIN_FILENO);
+// 		ft_close(pipefd[0]);
+// 		ft_close(pipefd[1]);
+// 		ft_dup2(backup, STDIN_FILENO);
+// 		close(backup);
+// 	}
+// }
+
 void	redirect(t_redirect *redi)
 {
 	int	pipefd[2];
-	int	backup;
 
-	if (redi->type == INPUT_REDI || redi->type == OUTPUT_REDI
-		|| redi->type == APPEND_OUTPUT_REDI)
+	if (redi->type == INPUT_REDI || redi->type == OUTPUT_REDI || redi->type == APPEND_OUTPUT_REDI)
 	{
-		redi->fd_backup = ft_dup(redi->fd);
-		ft_dup2 (redi->fd_file, redi->fd);
-		return ;
+		redi->fd_backup = ft_dup(redi->fd); // 元のファイルディスクリプタを保存
+		ft_dup2(redi->fd_file, redi->fd); // 新しいファイルディスクリプタにリダイレクト
 	}
-	else
+	else if (redi->type == HEREDOC_REDI)
 	{
-		backup = ft_dup(STDIN_FILENO);
-		ft_pipe(pipefd);
-		here_doc(redi, pipefd);
-		ft_dup2(pipefd[0], STDIN_FILENO);
+		here_doc(redi, pipefd); // ヒアドキュメントを処理
+		redi->fd_backup = ft_dup(STDIN_FILENO); // 元の標準入力を保存
+		ft_dup2(pipefd[0], STDIN_FILENO); // パイプから標準入力にリダイレクト
 		ft_close(pipefd[0]);
 		ft_close(pipefd[1]);
-		ft_dup2(backup, STDIN_FILENO);
-		close(backup);
 	}
 }
+
 
 int	exec_redirect(t_redirect *redi, t_directory *dir, t_env **env_var)
 {
@@ -95,13 +115,24 @@ int	exec_redirect(t_redirect *redi, t_directory *dir, t_env **env_var)
 	return (SUCCESS);
 }
 
+// void	restore_fd(t_redirect *redi)
+// {
+// 	if (redi == NULL)
+// 		return ;
+// 	if (redi->type == HEREDOC_REDI)
+// 		return ;
+// 	ft_dup2(redi->fd_backup, STDOUT_FILENO);
+// 	ft_close(redi->fd_backup);
+// 	redi->fd_backup = -1;
+// }
 void	restore_fd(t_redirect *redi)
 {
 	if (redi == NULL)
 		return ;
-	if (redi->type == HEREDOC_REDI)
-		return ;
-	ft_dup2(redi->fd_backup, STDOUT_FILENO);
-	ft_close(redi->fd_backup);
-	redi->fd_backup = -1;
+	if (redi->fd_backup != -1) // バックアップが有効な場合にのみ復元
+	{
+		ft_dup2(redi->fd_backup, redi->fd); // 元のファイルディスクリプタに戻す
+		ft_close(redi->fd_backup);
+		redi->fd_backup = -1; // 無効な状態にリセット
+	}
 }
